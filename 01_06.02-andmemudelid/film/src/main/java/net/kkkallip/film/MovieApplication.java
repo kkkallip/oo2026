3235717424e1f7c7e706c066858be0a9bc08ca9d
Karl K. Kallip <kkkallip@tlu.ee>
2026-02-13 10:20:47 +0200

    rename

--- 01_06.02-andmemudelid/film/src/main/java/net/kkkallip/film/MovieApplication.java
+++ 01_06.02-andmemudelid/film/src/main/java/net/kkkallip/film/MovieApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FilmApplication {
+public class MovieApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FilmApplication.class, args);
+		SpringApplication.run(MovieApplication.class, args);
 	}
 
 }
